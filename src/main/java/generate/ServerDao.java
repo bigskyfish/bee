468fd55eaec9218d17b357022dfa52ea4cf65057
fuchangbin <fuchangbin@corp.netease.com>
2021-06-06 00:27:54 +0800

    fix: bee 重启节点功能优化优化

---
+++ src/main/java/generate/ServerDao.java
@@ -0,0 +1,17 @@
+package generate;
+
+import generator.Server;
+
+public interface ServerDao {
+    int deleteByPrimaryKey(Integer id);
+
+    int insert(Server record);
+
+    int insertSelective(Server record);
+
+    Server selectByPrimaryKey(Integer id);
+
+    int updateByPrimaryKeySelective(Server record);
+
+    int updateByPrimaryKey(Server record);
+}
